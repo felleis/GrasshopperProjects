3a93354e3b724ecd02af6d855af00cbb98253ebb
felleis <96393742+felleis@users.noreply.github.com>
2022-03-17 22:25:55 +0100

    add html for easy access

--- keepItUp.js
+++ keepItUp.js
@@ -3,7 +3,11 @@ const svg = d3.select("#keepItUpBox");
 
 //creates balloon at random x position, falls on click
 
-let balloon = svg.append('circle').attr('fill', 'red').attr('r', 35).attr('cx', pickRandom(position)).attr('cy', 35);
+function pickRandom(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+let balloon = svg.append('circle').attr('fill', 'red').attr('r', 35).attr('cx', pickRandom(30, 240)).attr('cy', 35);
 balloon.on('click', () => {
-  balloon.transition().attr('cy', 165).duration(2000);
+  balloon.transition().ease(d3.easeQuadIn).attr('cy', 165).duration(2000).remove();
 });
