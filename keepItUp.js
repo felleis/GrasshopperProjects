508ba9092fbd0a067c57da6ca3d6afde845b8a18
felleis <96393742+felleis@users.noreply.github.com>
2022-03-17 08:09:49 +0100

    final puzzle for animations II

---
+++ keepItUp.js
@@ -0,0 +1,8 @@
+
+
+//creates balloon at random x position, falls on click
+
+let balloon = svg.append('circle').attr('fill', 'red').attr('r', 35).attr('cx', pickRandom(position)).attr('cy', 35);
+balloon.on('click', () => {
+  balloon.transition().attr('cy', 165).duration(2000);
+});
