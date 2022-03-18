c281ea604af797a5950c63f25d448582feaaf4b8
felleis <96393742+felleis@users.noreply.github.com>
2022-03-18 03:53:52 +0100

    added overview html and score for keepItUp minigame

--- cutTheCord.js
+++ cutTheCord.js
@@ -6,17 +6,18 @@ function randomColor (min, max) {
 	let randomRedValue = Math.floor(Math.random() * (max - min + 1) + min)
 	let randomGreenValue = Math.floor(Math.random() * (max - min + 1) + min)
 	let randomBlueValue = Math.floor(Math.random() * (max - min + 1) + min)
-	let randomResult = toString(randomBlueValue) && toString(randomGreenValue) && toString(randomRedValue)
-	return randomResult
+//	let randomResult = toString(randomBlueValue) && toString(randomGreenValue) && toString(randomRedValue)
+	return ["rgb(", randomRedValue,",", randomGreenValue, ",", randomBlueValue,"9"].join(""); 
 };
+
 
 
 console.log(randomColor(0, 255))
 
 
 var water = svg.append('rect').attr('width', 500).attr('height', 500).attr('y', 50).attr('fill', 'deepskyblue');
-var line = svg.append('rect').attr('width', 10).attr('height', 350).attr('x', 145).attr('y', 150).attr('fill', 'grey');
-var buoy = svg.append('circle').attr('r', 30).attr('cx', 150).attr('cy', 150).attr('fill', randomColor(0, 255));
+var line = svg.append('rect').attr('width', 7).attr('height', 250).attr('x', 145).attr('y', 250).attr('fill', 'grey');
+var buoy = svg.append('circle').attr('r', 30).attr('cx', 150).attr('cy', 250).attr('fill', randomColor(0, 255));
 function cutLine() {
   line.remove();
   buoy.transition().attr('cy', 70).duration(4000).ease(d3.easeElastic);
--- keepItUp.js
+++ keepItUp.js
@@ -7,10 +7,19 @@ function pickRandom(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+let score = 0 
+
 let balloon = svg.append('circle').attr('fill', 'red').attr('r', 35).attr('cx', pickRandom(30, 240)).attr('cy', 35);
 balloon.on('click', () => {
 	balloon.interrupt().attr('cy', 35).attr('cx', pickRandom(30, 240))
-  balloon.transition().ease(d3.easeQuadIn).attr('cy', 265).duration(2000).remove();
+  balloon.transition().ease(d3.easeQuadIn).attr('cy', 265).duration(2000).remove()
+  score = score + 10  
+  document.getElementById('score').textContent = score
 });
 
 
+
+// fix: show game over 
+// if (balloon.attr('cy',265)) {
+// 	console.log('Game over! Score:', score)
+// };
